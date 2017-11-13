fa9597dcd3427db0003550e6030d133b8c99d2a3
Paul Wambergue <paulwambergue@macbook-pro-de-paul.home>
2017-11-13 20:33:04 +0100

    factorizer is working

--- factorizer.py
+++ factorizer.py
@@ -1,116 +1,3 @@
-from collections import Counter
-from math import log
-from functools import reduce
-
-
-
-
-def countCiCjOccurences(s_c, Ci, Cj):
-    """
-        Nombre de fois ou s = CiCj apparait dans s_c
-    """
-    somme = 0
-    s = [Ci, Cj]
-    for mot in s_c:
-        somme += sum(mot[i:i + len(s)] == s for i in range(len(mot)))
-    return somme
-
-
-def count(s_c):
-    """
-        Renvoi un dictionnaire contenant le decompte de chaque motif
-        {"motif1": 25, "motif2": 30}
-    """
-    return reduce(lambda x, y: x + y, map(Counter, s_c))
-
-
-def descriptionLengthIncrease(s_c, Ci, Cj):
-    decomptes = count(s_c)
-    # Nombre total d'occurence
-    N = sum(decomptes.values())
-    N_Ci = decomptes[Ci]
-    N_Cj = decomptes[Cj]
-    N_CiCj = countCiCjOccurences(s_c,  Ci,  Cj)
-    N_bCiCj = N_Ci - N_CiCj
-    N_CibCj = N_Cj - N_CiCj
-
-    # Ci ou Cj ne sont pas des chunks - operation invalide
-    if N_Ci * N_Cj * N_CiCj == 0:
-        return 1000000
-
-    L1 = N_CiCj * (log((N + 3 - N_CiCj)/(N_CiCj + 1))
-                   - log(N/N_Ci) - log(N/N_Cj))
-    L2 = N_CibCj * (log((N + 3 + N_CiCj)/(N_CibCj + 1)) - log(N/N_Ci))
-    L2 += N_bCiCj * (log((N + 3 - N_CiCj)/(N_bCiCj + 1)) - log(N/N_Cj))
-    L3 = 3 * log(N + 3 - N_CiCj) - log((N_CiCj + 1) * (N_bCiCj + 1
-                                                       ) * (N_CibCj + 1))
-    L4 = (N - N_Ci - N_Cj) * log((N + 3 + N_CiCj)/N)
-    return L1 + L2 + L3 + L4
-
-
-def lexi(s_c):
-    return list(set(reduce(lambda x, y: x + y, s_c)))
-
-
-def replace_s_c(s_c, chunk1, chunk2):
-    sc_2 = list()
-    for mot in s_c:
-        mot2 = list(mot)
-        for i in range(len(mot2)-2):
-            if mot2[i] == chunk1 and mot2[i+1] == chunk2:
-                del mot2[i+1]
-                mot2[i] = chunk1 + chunk2
-        sc_2 += [mot2]
-    return sc_2
-
-
-class Optimizer:
-
-    def __init__(self):
-        self.scores = {}
-        self.toDo = []
-
-    def optimize(self, s_c):
-        self.toDo += [(0, s_c, set(), lexi(s_c))]
-
-        while self.toDo:
-            self.measure()
-
-        score_min = min(self.scores.keys())
-        return (score_min, self.scores[score_min])
-
-    def measure(self):
-
-        score, s_c, black_list, history = self.toDo.pop()
-        lex = lexi(s_c)
-        self.scores[score] = (lex, history)
-
-        for chunk1 in lex:
-            for chunk2 in lex:
-                if (chunk1, chunk2) not in black_list:
-                    interm_score = descriptionLengthIncrease(s_c,
-                                                             chunk1, chunk2)
-                    if interm_score <= 5:
-                        hist2 = list(history)
-                        hist2 += [(chunk1 + chunk2, chunk1, chunk2)]
-                        black_list2 = set(black_list)
-                        black_list2.add((chunk1, chunk2))
-                        new_score = score + interm_score
-                        new_s_c = replace_s_c(s_c, chunk1, chunk2)
-                        self.toDo += [(new_score, new_s_c, black_list2, hist2)]
-
-
-"""
-print("initialise Optimizer")
-opt = Optimizer()
-
-s = ["aab aaab abba baab"]
-s_c = [["a", "a", "b"], ["a", "a", "a", "b"], ["a", "b", "b", "a"
-                                               ], ["b", "a", "a", "b"]]
-print(opt.optimize(s_c))
-s = "45678 567 147 128 182 118 1132 321 21 2111 2112"
-"""
-
 
 class Factorizer:
 
@@ -127,21 +14,15 @@ class Factorizer:
         self.bestCost = 0
         residual = stimulus
         while(len(residual)!=0):
-
-            
-            i = 0
-            while (i < len(self.chunks)):
-                currentChunk = self.chunks[i]['word']
-                beginningOfResidual = residual[:min(len(residual),len(currentChunk))]
-                if currentChunk == beginningOfResidual: 
-                    self.bestCost += self.chunks[i]['codelength']
-                    self.bestFact += [currentChunk]
-                    residual = residual[len(currentChunk):]
+            previousResidual = residual
+            for chunk in self.chunks:
+                if chunk['word'] == residual[:min(len(residual),len(chunk['word']))]: 
+                    self.bestCost += chunk['codelength']
+                    self.bestFact += [chunk['word']]
+                    residual = residual[len(chunk['word']):]
                     break
-                else:
-                    i+=1
-
-
+            if residual and previousResidual == residual:
+                raise NameError("Can't factorize: C is incomplete (missing canonical chunks)")
 
     def factorize(self, stimulus):
 
@@ -149,64 +30,45 @@ class Factorizer:
         if (self.bestCost < 0):
             self.fastFactorize(stimulus)
 
-        # initializing toDo list
+        # initializing toDo tasks list
         usedChunks = []
         currentCost = 0
         self.toDo += [(stimulus, usedChunks, currentCost)]
 
         # exploring possibilities
-
         while self.toDo:
-
             residual, usedChunks, currentCost = self.toDo.pop()
-
             for chunk in self.chunks:
-
-                if chunk == residual[:min(len(residual),len(chunk))]:
-
-                    if (currentCost + chunk['codelength']) < self.bestCost:
-
-                        currentCost += chunk['codelength']
-                        residual = residual[len(chunk):]
-                        usedChunks += [chunk]
-
-                        if not residual: 
-                            if currentCost < self.bestCost:
-                                self.bestCost = currentCost
-                                self.bestFact = usedChunks
+                if residual and chunk['word'] == residual[:min(len(residual),len(chunk['word']))]:
+                    currentCost_temp = currentCost + chunk['codelength']
+                    if currentCost_temp < self.bestCost:
+                        residual_temp = str(residual[len(chunk['word']):])
+                        usedChunks_temp = list(usedChunks) + [chunk['word']]
+                        if not residual_temp: 
+                            if currentCost_temp < self.bestCost:
+                                self.bestCost = currentCost_temp
+                                self.bestFact = usedChunks_temp
                         else:
-                            self.toDo += [(residual, usedChunks, currentCost)]
+                            self.toDo += [(residual_temp, list(usedChunks_temp), currentCost_temp)]
 
 
+if __name__ == '__main__':
 
+    C = [
+    {'word': "aaaa", 'codelength':3},
+    {'word': "a", 'codelength':5}, 
+    {'word': "b", 'codelength':10}, 
+    {'word': "c", 'codelength':7}, 
+    {'word': "aab", 'codelength':1},
+    {'word': "cc", 'codelength':4}
+    ]
 
-C = [
-{'word': "aaaa", 'codelength':5},
-{'word': "a", 'codelength':2}, 
-{'word': "b", 'codelength':1}, 
-{'word': "c", 'codelength':7}, 
-{'word': "ccc", 'codelength':10}
-]
+    stimulus = "accaaabaacaa"
 
-C = {}
+    print("initialise Factorizer")
+    fact = Factorizer(C)
+    fact.factorize(stimulus)
 
-stimulus = "aaaabcccaa"
-
-print("initialise Factorizer")
-fact = Factorizer(C)
-fact.factorize(stimulus)
-
-print(stimulus)
-print(fact.bestCost)
-print(fact.bestFact)
-
-
-
-
-
-
-
-
-
-
-
+    print("C = ", C)
+    print("stimulus = ", stimulus)
+    print("Factorization = ", fact.bestFact, " (cost: ", fact.bestCost, ")")
