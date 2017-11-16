e38c95931cc72a4303f1d01ec5a0086f78fdc1b2
Raphaël <raphael.gavache@student.ecp.fr>
2017-11-16 12:02:14 +0100

    main

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 from MDLChunker import MDLChunker_search_factorized_distribution as mdla
+from optimization import lexi
 
 
-print(mdla("blablabla blablabla", 1))
+stimulis = ""
+with open('data.txt', 'r') as myfile:
+        stimulis = myfile.read().replace('\n', '')
+
+
+
+
+C, S_C, total = mdla(stimulis[1:200], 1)
+print(lexi(S_C))
