55a249bbdd8aad42ca483c1470a2cd26f6bc4dad
Raphaël <raphael.gavache@student.ecp.fr>
2017-11-16 12:27:30 +0100

    Paul's main

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 from MDLChunker import MDLChunker_search_factorized_distribution as mdla
-from optimization import lexi
+from MDLChunker import MDLChunker_search_factorized as mdl
+from fonctions_aux import extract_words
 
+"""
+    string = "aaa aaabbb bbbaaabbb bbb bbb aaabbb bbbbbbaaa bbbaaaaaabbb aaa bbb \
+    aaabbb bbbaaa aaaaaa bbbaaa aaa aaabbb bbbaaabbb bbb bbb aaabbb bbbbbbaaa \
+    bbbaaaaaabbb aaa bbb aaabbb bbbaaa aaaaaa bbbaaa"
+    """
 
-stimulis = ""
-with open('data.txt', 'r') as myfile:
-        stimulis = myfile.read().replace('\n', '')
+#string = "blablabla blablabla"
 
+string = "clakjo kjo kjo kjoclakjo kjoclakjokjo clakjocla kjo cla claclakjo clacla"
 
+alpha = 1.
 
+all_stimuli = extract_words(string)
 
-C, S_C, total = mdla(stimulis[1:200], 1)
-print(lexi(S_C))
+C, S_C, TOTAL = mdla(all_stimuli, alpha)
+#C, S_C, TOTAL = mdl(all_stimuli)
+
+print("\nstring = ", string)
+print("alpha = ", alpha)
+print("\nC =  ", C)
+print("\nS_C =  ", S_C)
+print("\nTOTAL =  ", TOTAL)
