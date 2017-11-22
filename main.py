496db2a675f228e2a1290ff174b6f322cb4d656f
Paul Wambergue <paulwambergue@macbook-pro-de-pw.home>
2017-11-22 17:35:35 +0100

    last commit

--- main.py
+++ main.py
@@ -2,15 +2,14 @@ from MDLChunker import MDLChunker_search_factorized_distribution as mdla
 from MDLChunker import MDLChunker_search_factorized as mdl
 from fonctions_aux import extract_words
 
-"""
-    string = "aaa aaabbb bbbaaabbb bbb bbb aaabbb bbbbbbaaa bbbaaaaaabbb aaa bbb \
-    aaabbb bbbaaa aaaaaa bbbaaa aaa aaabbb bbbaaabbb bbb bbb aaabbb bbbbbbaaa \
-    bbbaaaaaabbb aaa bbb aaabbb bbbaaa aaaaaa bbbaaa"
-    """
+
+string = "aaa aaabbb bbbaaabbb bbb bbb aaabbb bbbbbbaaa bbbaaaaaabbb aaa bbb \
+aaabbb bbbaaa aaaaaa bbbaaa aaa aaabbb bbbaaabbb bbb bbb aaabbb bbbbbbaaa \
+bbbaaaaaabbb aaa bbb aaabbb bbbaaa aaaaaa bbbaaa"
 
 #string = "blablabla blablabla"
 
-string = "clakjo kjo kjo kjoclakjo kjoclakjokjo clakjocla kjo cla claclakjo clacla"
+#string = "clakjo kjo kjo kjoclakjo kjoclakjokjo clakjocla kjo cla claclakjo clacla"
 
 alpha = 1.
 
